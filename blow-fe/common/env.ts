741ebe437874c4ffacc32f37f92155447456e40a
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-10-13 05:50:36 +0200

    Update env.ts

--- blow-fe/common/env.ts
+++ blow-fe/common/env.ts
@@ -9,7 +9,7 @@ const getEnv = (key: string, fallback?: string) => {
 const apiUrl =
   getEnv("NEXT_PUBLIC_API_URL") ||
   getEnv("NEXT_PUBLIC_BACKEND_URL") ||
-  "http://api.kutumba.ru/api";
+  "https://api.kutumba.ru/api";
 
 const mediaUrl =
   getEnv("NEXT_PUBLIC_MEDIA_URL") ||
@@ -18,7 +18,7 @@ const mediaUrl =
 export const config = {
   API_URL: apiUrl,
   MEDIA_URL: mediaUrl,
-  NEXT_PUBLIC_APP_URL: getEnv("NEXT_PUBLIC_APP_URL", "http://kutumba.ru"),
+  NEXT_PUBLIC_APP_URL: getEnv("NEXT_PUBLIC_APP_URL", "https://kutumba.ru"),
   NEXT_PUBLIC_API_URL: apiUrl,
   NEXT_PUBLIC_YOOMONEY_CLIENT_ID: getEnv("NEXT_PUBLIC_YOOMONEY_CLIENT_ID"),
   NEXT_YOOMONEY_CLIENT_SECRET: getEnv("NEXT_YOOMONEY_CLIENT_SECRET"),
